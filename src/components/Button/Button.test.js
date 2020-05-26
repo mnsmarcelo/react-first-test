6fc92dc4370ea6ac4eb31b353c6f19430b8653db
Marcelo Sousa <mns.marcelo67@gmail.com>
2020-05-26 10:45:04 -0300

    refactor: change test to testing-library/react

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { render, fireEvent  } from '@testing-library/react';
 
 import Button from './Button';
 
@@ -8,15 +8,17 @@ let wrapped = null;
 describe('Test Button', () => {
     const clickMock = jest.fn();
     beforeEach(() => {
-        wrapped = shallow(<Button onClick={clickMock} />);
+        wrapped = render(<Button onClick={clickMock} />);
     });
 
     test('Should exists one instance off Button', () => {
-        expect(wrapped.exists()).toBeTruthy();
+        const { getByRole } = wrapped;
+        expect(getByRole('button')).toBeTruthy();
     });
 
     test('Should component receive one click', () => {
-        wrapped.find('button').simulate('click');
+        const { getByRole } = wrapped;
+        fireEvent.click(getByRole('button'));
         expect(clickMock.mock.calls.length).toBe(1);
     });
 });
